e7f4c4548099d6ee203ba020c7f347eb32b9fc78
shri0915 <shrijit.panicker@gmail.com>
2021-02-12 19:17:28 +0530

    Days Work

--- src/components/Organisms/InformationForm.js
+++ src/components/Organisms/InformationForm.js
@@ -1,65 +1,151 @@
 import React from 'react';
-import { Button, Form, TextInput, TextArea, Select, SelectItem, text } from "carbon-components-react";
+import { Button, Form, TextInput, TextArea, Select, SelectItem } from "carbon-components-react";
 import {createUseStyles} from 'react-jss';
-
+import stateList from "../../data/StateList";
 
 const useStyle = createUseStyles({
+  informationForm:{
+    display: 'Flex',
+    alignContent: 'center',
+    backgroundColor: 'white',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    marginTop: '10vw',
+    marginBottom: '10vw',
+    marginRight: '25vw',
+    marginLeft: '25vw',
+    textAlign: 'left'
+  },
 submitButton:{
   backgroundColor: 'purple',
   fontWeight: 'bold',
   alignContent: 'center',
   textAlign: 'center'
+},
+lableTextMobile:{
+  color: '#525252',
+  fontSize: '10pt'
 }
-
 })
 
 
 const InformationForm = ({children}) => {
   const classes = useStyle()
 return (
-  <Form id="FormInput">
-    <div 
-
-    />
-    <div style={{marginBottom: '2rem'}}>
+  <Form className={classes.informationForm}>
+    
+    <div style={{margin: '2rem'}}>
       <TextInput
-        id="test2"
+        id="firstName"
         invalidText="Invalid error message."
-        labelText="Name"
-        placeholder="Please enter your name"
+        labelText="First Name"
+        placeholder="Johnny"
+        required
       />
     </div>
-    <div style={{marginBottom: '2rem'}}>
+    <div style={{margin: '2rem'}}>
+      <TextInput
+        id="lastName"
+        invalidText="Invalid error message."
+        labelText="Last Name"
+        placeholder="Appleseed"
+        required
+      />
+      </div>
+      <div style={{margin: '2rem'}}>
+      <TextInput
+        id="dob"
+        invalidText="Invalid error message."
+        labelText="Date Of Birth"
+        placeholder="MM/DD/YYYY"
+        required
+      />
+      </div>
+      <div style={{margin: '2rem'}}>
+      <TextInput
+        id="ssn"
+        invalidText="Invalid error message."
+        labelText="Social Security Number (Last 4 Digits, optional)"
+        placeholder="####"
+      />
+      </div>
+      <div style={{margin: '2rem'}}>
+      <labelText className={classes.lableTextMobile}>Mobile Number</labelText>
+      <TextInput
+        id="mobile"
+        invalidText="Invalid error message."
+        labelText="To receive text messages and download our mobile application"
+        placeholder="(###)###-####"
+        required
+      />
+      
+      </div>
+      <div style={{margin: '2rem'}}>
+      <TextInput
+        id="email"
+        type="email"
+        invalidText="Invalid error message."
+        labelText="Email"
+        placeholder="you@mail.com"
+        required
+        pattern='\S+@\S+\.\S+'
+      />
+      </div>
+    <div style={{margin: '2rem'}}>
     <TextArea
       cols={50}
-      id="test5"
+      id="mailingAddress1"
       invalidText="Invalid error message."
-      labelText="Address"
-      placeholder="Please enter your address"
-      rows={4}
+      labelText="Mailing Address"
+      placeholder="Primary street address"
+      rows={1}
+      required
     />
     </div>
-    <div style={{marginBottom: '2rem'}}>
+    <div style={{margin: '2rem'}}>
+    <TextArea
+      cols={50}
+      id="mailingAddress2"
+      invalidText="Invalid error message."
+      labelText="Mailing Address (optional)"
+      placeholder="Unit #, Apt, Suite"
+      rows={1}
+    />
+    </div>
+    <div style={{margin: '2rem'}}>
+      <TextInput
+        id="city"
+        invalidText="Invalid error message."
+        labelText="City"
+        placeholder="Seattle"
+        required
+      />
+      </div>
+    <div style={{margin: '2rem'}}>
       <Select
-        defaultValue="Select Country"
+        defaultValue="State"
         id="select-1"
         invalidText="This is an invalid error message."
-        labelText="Select Country"
+        labelText="State"
+        required
       >
-        <SelectItem
-          text="India"
-          value="India"
-        />
-        <SelectItem
-          text="France"
-          value="France"
-        />
-        <SelectItem
-          text="Japan"
-          value="Japan"
-        />
+        {stateList.map((state) =>
+          <SelectItem 
+          text = {state.abbr}
+          value = {state.name}
+          />
+        )}
       </Select>
     </div>
+    <div style={{margin: '2rem'}}>
+      <TextInput
+        id="postalCode"
+        invalidText="Invalid error message."
+        labelText="Postal Code"
+        placeholder="#####"
+        required
+      />
+      </div>
     <Button
       className={classes.submitButton}
       kind="primary"
